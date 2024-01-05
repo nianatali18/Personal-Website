f2d1e4089b580552579db6baece35f0ceec42b15
nia <nianatali@gmail.com>
2024-01-04 22:50:41 -0800

    Update data.js

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,11 +1,9 @@
 import { SiJavascript, SiMongodb, SiMysql } from "react-icons/si";
 import { FaNodeJs } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
 
 const projects = [
-  { id: 1, url: '', title: '', description: '', image: './weather.jpg', icons: [SiJavascript] },
-  { id: 2, url: '', title: '', description: '', image: './api.jpg', icons: [FaNodeJs, SiMongodb] },
-  { id: 3, url: '', title: '', description: '', image: './movies.jpg', icons: [SiJavascript] },
-  { id: 4, url: '', title: '', description: '', image: './game-yap.jpg', icons: [SiJavascript, SiMysql] }
+  { id: 1, url: 'www.linkedin.com/in/nia-alvarado-603082155', title: 'LinkedIn', description: '', image: './weather.jpg', icons: { FaLinkedin } },
 ];
 
 export default projects;
